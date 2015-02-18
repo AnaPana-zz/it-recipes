0e83b1df3cb3b567f0470d8a6db2decc3251a8e6
AnaPana <admin@it-recipes.com>
2015-02-18 19:46:09 +0200

    .gitignore is now working

--- itblog/credentials.py
+++
@@ -1,6 +0,0 @@
-ADMIN_MAIL = {
-              'address'  : 'admin@it-recipes.com',
-              'password' : 'MyNewItRecipes',
-              'server'   : 'smtp.mail.ru',
-              'port'     : '2525',
-              }
