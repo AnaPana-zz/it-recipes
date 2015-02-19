3e781ab4d04f270f282dc5a436ac775e622b8722
AnaPana <admin@it-recipes.com>
2015-02-19 02:35:05 +0200

    Add captcha to installed apps

--- itblog/itblog/settings.py
+++ itblog/itblog/settings.py
@@ -155,6 +155,7 @@ INSTALLED_APPS = (
     'main',
     'dashboard',
     'tasks',
+    'captcha',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
