fe15b4a6278f52818d5750759e752710a4a0abba
Panchenko, Anastasiia <anastasiia.panchenko@nih.gov>
2017-09-26 07:36:29 -0400

    First step to Django 1.11

--- itblog/itblog/urls.py
+++ itblog/itblog/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', 'main.views.home', name='home'),
     url(r'^subject$', 'main.views.subjects', name='subjects'),
     url(r'^about$', 'main.views.about', name='about'),
@@ -14,32 +13,30 @@ urlpatterns = patterns('',
     url(r'^login$', 'main.login_views.login_user', name='login'),
     url(r'^logout', 'main.login_views.logout_user', name='logout'),
     url(r'^captcha/', include('captcha.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+]
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
-
-articles = patterns('',
+articles = [
     url(r'^(?P<page_type>tags|subjects|search|recent)/(?P<item_name>[A-Za-z0-9_\-\.&% ]+)$', 'main.views.main', name='main'),
     url(r'^(?P<page_type>tags|subjects|search|recent)/(?P<item_name>[A-Za-z0-9_\-\.&% ]+)/(?P<page>\d+)/(?P<delta>\d+)$', 'main.views.main', name='main'),
     url(r'^blog/(?P<article_id>\d+)$', 'main.views.blog', name='blog'),
-            )
+]
 
-article = patterns('',
+article = [
     url(r'^add$', 'main.views.add_article', name='addarticle'),
     url(r'^edit/(?P<article_id>\d+)$', 'main.views.edit_article', name='editarticle'),
     url(r'^preview/(?P<article_id>\d+)$', 'main.views.preview_article', name='previewarticle'),
-           )
+]
 
-comments = patterns('',
+comments = [
     url(r'^answer_to/(?P<comment_id>\d+)$', 'main.views.comment_dialog', name='commentdialog'),
     url(r'^edit/(?P<comment_id>\d+)$', 'main.views.edit_comment', name='editcomment'),
-            )
+]
 
-urlpatterns += patterns('',
+urlpatterns += [
     (r'^articles/', include(articles)),
     (r'^article/', include(article)),
     (r'^comments/', include(comments)),
     (r'^dashboard/', include('dashboard.urls')),
     (r'^tasks/', include('tasks.urls')),
-)
+]
